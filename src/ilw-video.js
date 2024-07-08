7c615fc68abbb92ae747dcc17974d326653924a7
Andrew Reynolds <areynold@illinois.edu>
2024-07-08 15:05:36 -0500

    stub iframe elements

--- src/ilw-video.js
+++ src/ilw-video.js
@@ -8,6 +8,7 @@ class Video extends LitElement {
             align: { type: String, attribute: true },
             focus: { type: String, attribute: true },
             theme: { type: String, attribute: true },
+            url: { type: String, attribute: true },
         };
     }
 
@@ -20,11 +21,13 @@ class Video extends LitElement {
         this.align = '';
         this.focus = false;
         this.theme = '';
+        this.url = '';
     }
 
     render() {
         return html`
             <div class="video">
+                <iframe id="todo_generate_id_from_url" width="640" height="394" src="todo_pass_url" class="todo_check_embed_class" todo_generate_options frameborder="0" title="todo_generate_title" style="></iframe>
             </div>
         `;
     }
