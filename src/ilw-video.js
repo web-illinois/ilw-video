c6c657952eb099e0a7ac395293152cad9c1dc742
Andrew Reynolds <areynold@illinois.edu>
2024-09-03 13:07:47 -0500

    stub iframe generator

--- src/ilw-video.js
+++ src/ilw-video.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'lit';
 import styles from './ilw-video.styles'
 import './ilw-video.css';
+import UrlItem from './legacy/urlitem';
 
 class Video extends LitElement {
     static get properties() {
@@ -29,6 +30,16 @@ class Video extends LitElement {
     render() {
         const aspectOverride = this.aspectratio ? `--ilw-video--aspect-ratio: ${this.aspectratio}` : '';
 
+        // check slot vs. src
+        // if slot
+        // get dimensions
+        // override aspect, dimensions
+        // return html
+        // else if src
+        // get src, title
+        // generate embed
+        // return html
+
         const dimensions = this.getIframeDimensions();
         this.height = this.height ? this.height : dimensions.height;
         this.width = this.width ? this.width : dimensions.width;
@@ -42,9 +53,20 @@ class Video extends LitElement {
         `;
     }
 
-    pixelate(dimension) {
-        const pixelated = isNaN(dimension) ? dimension : dimension + 'px';
-        return pixelated;
+    generateIframe(url, title, view) {
+        console.warn(`Generating iframe for ${title}`);
+        let urlHelper = new UrlItem.UrlItem(url, view);
+        if (urlHelper.videoType == "youtube") {
+            return html`<iframe title='${title} (video)' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' src='${urlHelper.videoUrl}' frameborder='0' allowfullscreen></iframe>`;
+        } else if (urlHelper.videoType == "mediaspace") {
+            return html`<iframe title='${title} (video)' id='kaltura_player_${urlHelper.videoId}' class='kmsembed' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' src='${urlHelper.videoUrl}' style='float: left; margin: 10px 10px 10px 0;' allowfullscreen webkitallowfullscreen mozAllowFullScreen allow='autoplay *; fullscreen *; encrypted-media *' frameborder='0'></iframe>`;
+        } else if (urlHelper.videoType == "vimeo") {
+            return html`<iframe title='${title} (video)' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' src='${urlHelper.videoUrl}' frameborder='0' allowfullscreen></iframe>`;
+        } else if (urlHelper.videoType == "blank") {
+            return html`<div style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: black; color: white; display: flex; justify-content: center; align-items: center; font-weight: bold;'>${title}</div>`;
+        } else {
+            return '';
+        }
     }
 
     getIframeDimensions() {
@@ -62,6 +84,11 @@ class Video extends LitElement {
         };
 
         return dimensions;
+    }
+
+    pixelate(dimension) {
+        const pixelated = isNaN(dimension) ? dimension : dimension + 'px';
+        return pixelated;
     }
 }
 
