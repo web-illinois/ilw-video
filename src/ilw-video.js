3aa588dd22477fb33faa80740292485e2fdade9f
Andrew Reynolds <areynold@illinois.edu>
2024-09-05 15:41:54 -0500

    extract aspect ratio converter

--- src/ilw-video.js
+++ src/ilw-video.js
@@ -29,7 +29,7 @@ class Video extends LitElement {
     }
 
     render() {
-        const inlineAspect = this.aspectratio ? `--ilw-video--aspect-ratio: ${this.convertAspectRatio(this.aspectratio)}` : '';
+        const inlineAspect = this.aspectratio ? `--ilw-video--aspect-ratio: ${AttributeUtils.convertAspectRatio(this.aspectratio)}` : '';
 
         const embed = this.querySelector('iframe, embed, object');
 
@@ -56,19 +56,6 @@ class Video extends LitElement {
                 </div>
             </div>
         `;
-    }
-
-    convertAspectRatio(aspect) {
-        switch (aspect) {
-            case aspect === 'tv':
-                console.warn('Legacy aspect ratio "tv" is deprecated. Converting to 16/9.');
-                return '16/9';
-            case aspect === 'vertical':
-                console.warn('Legacy aspect ratio "vertical" is deprecated. Converting to 9/16.');
-                return '9/16';
-            default:
-                return aspect;
-        }
     }
 
     generateIframe(url, title, view) {
--- src/utilities/attribute-utils.js
+++ src/utilities/attribute-utils.js
@@ -1,5 +1,18 @@
 class Item { }
 export default class AttributeUtils extends Item {
+    static convertAspectRatio(aspect) {
+        switch (aspect) {
+            case aspect === 'tv':
+                console.warn('Legacy aspect ratio "tv" is deprecated. Converting to 16/9.');
+                return '16/9';
+            case aspect === 'vertical':
+                console.warn('Legacy aspect ratio "vertical" is deprecated. Converting to 9/16.');
+                return '9/16';
+            default:
+                return aspect;
+        }
+    }
+
     static pixelate(dimension) {
         const pixelated = isNaN(dimension) ? dimension : dimension + 'px';
         return pixelated;
